e59c1a446246a0b1b0d8edd9c2ec72d043e2a5d6
Lupita Reyes <lupita.g.reyes@outlook.com>
2023-03-22 14:57:22 -0400

    Build button using storybook

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,24 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  let buttonClass = "button";
+  // if confirm is passed as prop, then add CSS class styles to the confirm buttons
+
+  if (props.confirm) {
+    buttonClass += " button--confirm"
+  }
+  if (props.danger) {
+    buttonClass += " button--danger"
+  }
+
+
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  )
 }
