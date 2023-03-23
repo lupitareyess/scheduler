a76e5a8842fde56d6380451268b0ab20b6feb98a
Lupita Reyes <lupita.g.reyes@outlook.com>
2023-03-23 15:30:47 -0400

    Add DayList component, update App file

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,13 +9,27 @@ export default function DayListItem(props) {
 
   const dayClass = classNames('day-list__item', { 'day-list__item--selected': selected, 'day-list__item--full': spots === 0 })
 
+  // could probably move this to helper function module
+  function formatSpots(spots) {
+    if (spots === 0) {
+      return 'no spots remaining'
+    }
 
+    if (spots === 1) {
+      return '1 spot remaining'
+    }
+
+    if (spots > 1) {
+      return `${spots} spots remaining`
+    }
+
+  }
 
   return (
-    <li className={dayClass}>
-      <h2 className="text--regular" onClick={() => setDay(name
-      )}>{name}</h2>
-      <h3 className="text--light">{spots} spots remaining</h3>
+    <li className={dayClass} onClick={() => setDay(name
+    )}>
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
   );
 }
